4809b30403db01d2b39b1777d9164830cf92a105
venkateshvenkynallana <venkateshnallana2003@gmail.com>
2026-02-11 17:02:34 +0530

    register the user get mail

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,13 @@
 
 import express from 'express';
-import { getAllUsers } from '../controller/getAllUsersController.js';
+import { getAllUsers, getUsersInUserDashboard } from '../controller/getAllUsersController.js';
 import { protectAdminRoute } from '../middleware/auth.js';
+import { protectRoute } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/users", protectAdminRoute, getAllUsers);
+
+router.get("/userscount", protectRoute, getUsersInUserDashboard);
 
 export default router;
