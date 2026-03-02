6d8f8b82425a465b223317db4bb173320b11671a
venkateshvenkynallana <venkateshnallana2003@email.com>
2026-03-02 12:47:28 +0530

    connect status

--- controller/connections/getFindDoctorDetails.js
+++ controller/connections/getFindDoctorDetails.js
@@ -2,21 +2,39 @@
 import { decrypt } from "../../lib/encrypt.js";
 import { decodeToken } from "../../lib/utils.js";
 import User from "../../models/User.js";
+import Connections from "../../models/Connection.js";
 
 
 export const getFindDoctorDetails = async(req, res)=>{
     try {
             const {userId} = decodeToken(req);
+             const connections = await Connections.find({
+                $or: [
+                    { sender: userId },
+                    { receiver: userId }
+                ]
+                });
+                const connectionMap = {};
+                connections.forEach((conn) => {
+                let otherUserId;
+                if (conn.sender.toString() === userId) {
+                    otherUserId = conn.receiver.toString();
+                } else {
+                    otherUserId = conn.sender.toString();
+                }
+                connectionMap[otherUserId] = conn.status;
+                });
             const users = await User.find(
                 {_id: {$ne: userId}}
             ).select("-password");
             const decryptedData = users.map((user) => {
-                
+                const status = connectionMap[user._id.toString()] || null;
                 return {
                     fullName:user?.fullName,
                     designation:(user?.designation)?decrypt(user.designation):null,
                     profilepic: user?.profilepic,
-                    _id: user?._id
+                    connectionStatus: status,
+                    _id: user?._id,
                 };  
             });
             console.log("decryptedData",decryptedData)
